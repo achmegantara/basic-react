107da9f3276cceaee1598bcfdb96b487c9658d37
achmegantara <achmegantara@gmail.com>
2021-02-20 20:55:00 +0700

    design web

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 
 function App() {
 
+  const category = "RUNNING";
+  const price = 74000000;
+
   return (
-    <div>
-      <h1>
-        Belajar with ega
-      </h1>
-      <p>
-        Meningkatkan keahlian design dan coding secara onling dan cepat.
-      </p>
+    <div class="Parentbox">
+      <div class="Foto">
+      <img src="foto-sneakers.jpeg"></img>
+      </div>
+      <div class="Deskripsi">
+        <p class="Cate">{category}</p>
+        <h1 class="Title">Pinky Swagg</h1>
+        <p class="Price">IDR {price}</p>
+        <p class="info">
+          One of the most recognizable shoes in the AJ collections, the Air Jordan Retro features lightweight, visible cushioning just like the original from '88. 
+        </p>
+      </div>
     </div>
   );
 }
