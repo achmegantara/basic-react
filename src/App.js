979b9d4df32c88bad5bc4243af54c8531702a39f
achmegantara <achmegantara@gmail.com>
2021-02-20 23:15:37 +0700

    props component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="Parentbox">
-
+      <FotoProduk />
+      <ProdukInfo name="Purple Swag" category="LEBARAN"/>
     </div>
   );
 }
@@ -17,14 +18,15 @@ function FotoProduk(){
   );
 }
 
-function ProdukInfo() {
-  const category = "RUNNING";
+function ProdukInfo(props) {
+  //object destruction =
+  const { category, name } = props;
   const price = 74000000;
   return (
     <div>
       <div className="Deskripsi">
         <p className="Cate">{category}</p>
-        <h1 className="Title">Pinky Swagg</h1>
+        <h1 className="Title">{name}</h1>
         <p className="Price">IDR {price}</p>
         <p className="info">
           One of the most recognizable shoes in the AJ collections, the Air Jordan Retro features lightweight, visible cushioning just like the original from '88. 
