7e6d7488c1caa344e679fcd0b98947f5521ba174
achmegantara <achmegantara@gmail.com>
2021-02-20 23:04:25 +0700

    slicing component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  return (
+    <div className="Parentbox">
 
+    </div>
+  );
+}
+
+function FotoProduk(){
+  return (
+    <div className="Foto">
+    <img src="foto-sneakers.jpeg"></img>
+    </div>
+  );
+}
+
+function ProdukInfo() {
   const category = "RUNNING";
   const price = 74000000;
-
   return (
-    <div class="Parentbox">
-      <div class="Foto">
-      <img src="foto-sneakers.jpeg"></img>
-      </div>
-      <div class="Deskripsi">
-        <p class="Cate">{category}</p>
-        <h1 class="Title">Pinky Swagg</h1>
-        <p class="Price">IDR {price}</p>
-        <p class="info">
+    <div>
+      <div className="Deskripsi">
+        <p className="Cate">{category}</p>
+        <h1 className="Title">Pinky Swagg</h1>
+        <p className="Price">IDR {price}</p>
+        <p className="info">
           One of the most recognizable shoes in the AJ collections, the Air Jordan Retro features lightweight, visible cushioning just like the original from '88. 
         </p>
       </div>
