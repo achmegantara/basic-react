a4675254a189714dad5b0544b76bd03e89d44c0b
achmegantara <achmegantara@gmail.com>
2021-02-21 19:02:57 +0700

    Map JSON review

--- src/App.js
+++ src/App.js
@@ -6,41 +6,49 @@ function App() {
     <div className="Parentbox">
       <FotoProduk />
       <ProdukInfo isDiscount="coming" name="Purple Swag" category="LEBARAN"/>
-
-    <div className="Review-box">
-      <h2>Reviews</h2>
-      <div className="Item">
-      <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"></img>
-      <div className="User">
-        <h3>Masayoshi Setiawan</h3>
-        <p>Harganya murah tapi kualitas bukan kaleng-kaleng nih.</p>
-      </div>
-      </div>
-    </div>
-
+      <ReviewItems />
     </div>
   );
 }
 
 function ReviewItems(){
   // mempersiapkan data dummy JSON
-  const users = [
+  const reviews = [
     {
       "id": 1,
       "name": "Masayoshi",
-      "review": "Harganya murah, kualitas bagus"
+      "review": "Harganya murah, kualitas bagus",
+      "link" : "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
     },
     {
       "id": 2,
       "name": "Achmad",
-      "review": "Harganya merakyat"
+      "review": "Harganya merakyat",
+      "link" : "https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     },
     {
       "id": 3,
       "name": "Akbar",
-      "review": "Hbarangnya bagus, perngiriman cepat."
+      "review": "barangnya bagus, perngiriman cepat.",
+      "link" : "https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     }
   ];
+  const ListReviews = reviews.map((itemReview) => 
+  <div className="Item">
+  <img src={itemReview.link}></img>
+  <div className="User">
+    <h3>{itemReview.name}</h3>
+    <p>{itemReview.review}</p>
+  </div>
+  </div>
+);
+  return (
+    <div className="Review-box">
+      <h2>Reviews</h2>
+      {ListReviews}
+    </div>
+
+  );
 }
 
 function FotoProduk(){
