b4507ffc44df85ec85dd9cba14abe9cfcb2b8812
achmegantara <achmegantara@gmail.com>
2021-02-21 18:19:19 +0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="Parentbox">
       <FotoProduk />
-      <ProdukInfo name="Purple Swag" category="LEBARAN"/>
+      <ProdukInfo isDiscount="coming" name="Purple Swag" category="LEBARAN"/>
     </div>
   );
 }
@@ -18,9 +18,28 @@ function FotoProduk(){
   );
 }
 
+function CheckDiscount(props){
+  const { isDiscount } = props;
+  if(isDiscount === "yes"){
+    return (
+      <p>Diskon 50%</p>
+    );
+  } 
+  else if (isDiscount === "coming") {
+    return (
+    <p>Akan ada diskon...</p>
+    );
+  }
+  else {
+    return (
+    <p>Belum ada diskon</p>
+    );
+  }
+}
+
 function ProdukInfo(props) {
-  //object destruction =
-  const { category, name } = props;
+  //object destruction
+  const { category, name, isDiscount } = props;
   const price = 74000000;
   return (
     <div>
@@ -28,6 +47,7 @@ function ProdukInfo(props) {
         <p className="Cate">{category}</p>
         <h1 className="Title">{name}</h1>
         <p className="Price">IDR {price}</p>
+        <CheckDiscount isDiscount={isDiscount} />
         <p className="info">
           One of the most recognizable shoes in the AJ collections, the Air Jordan Retro features lightweight, visible cushioning just like the original from '88. 
         </p>
